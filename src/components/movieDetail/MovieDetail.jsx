1967fccb7c3439aaa175fd1bdf62aa9f8ca7067e
Paul_Delamare <paulodela2006@gmail.com>
2025-07-22 16:47:35 +0200

    Refactor le composant MovieDetail pour intégrer le bouton de gestion de wishlist et simplifier le code en supprimant les fonctions de wishlist redondantes.

--- src/components/movieDetail/MovieDetail.jsx
+++ src/components/movieDetail/MovieDetail.jsx
@@ -6,11 +6,12 @@ import { getImageUrl } from "../../services/movieApi";
 import styles from "./MovieDetail.module.css";
 import SimilarMovies from "../similarMovies/SimilarMovies";
 import ActorList from "../actorList/ActorList";
+import WishlistManagementButton from "../utils/wishListManagementButton/WishlistManagementButton";
 
 export default function MovieDetail() {
   const { id } = useParams();
-  const { wishlist, onAddToWishlist, removeFromWishlist } =
-    useContext(WishlistContext);
+  const { wishlist } = useContext(WishlistContext);
+
   const { movie, actors, loading, error } = useMovieDetail(id);
 
   if (loading) return <div className={styles.loading}>Chargement…</div>;
@@ -50,30 +51,7 @@ export default function MovieDetail() {
 
         <ActorList actors={actors} />
 
-        <div className={styles.wishlist}>
-          {inWishlist ? (
-            <button
-              className={styles.wishlistBtnDelete}
-              onClick={() => removeFromWishlist(movie.id)}
-            >
-              <svg
-                className={styles.trashIcon}
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 448 512"
-              >
-                <path d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z" />
-              </svg>
-              Retirer de la Wishlist
-            </button>
-          ) : (
-            <button
-              className={styles.wishlistBtn}
-              onClick={() => onAddToWishlist(movie)}
-            >
-              Ajouter à la Wishlist
-            </button>
-          )}
-        </div>
+        <WishlistManagementButton inWishlist={inWishlist} movie={movie} />
       </div>
 
       <SimilarMovies id={movie.id} />
